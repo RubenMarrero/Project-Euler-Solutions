76400fbcc721fc8e947dd54aa600dacf1ad06f95
Ruben Marrero <rubenmarreroruiz@gmail.com>
2021-01-12 23:53:43 +0000

    Problem #1 Solved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function secuencia(n,d) { return d + (n-1) * d; }
 function sumatorio_secuencia(n,a1,an){ return (n/2)*(a1+an) }
 
-const max_int = 10;
+const max_int = 999;
 
 let d = 3;
 let n = parseInt(max_int/d);
@@ -9,7 +9,7 @@ let n = parseInt(max_int/d);
 const sum_multiplos_3 = sumatorio_secuencia(
     n,
     d,
-    (secuencia(n,d)/d)
+    secuencia(n,d)
 );
 
 d = 5;
@@ -18,10 +18,18 @@ n = parseInt(max_int/d);
 const sum_multiplos_5 = sumatorio_secuencia(
     n,
     d,
-    secuencia(n,d)/d
+    secuencia(n,d)
 );
 
-console.log(sum_multiplos_5);
+d = 15;
+n = parseInt(max_int/d);
+const sum_multiplos_15 = sumatorio_secuencia(
+    n,
+    d,
+    secuencia(n,d)
+);
+
 console.log(sum_multiplos_3);
-console.log(sum_multiplos_5+sum_multiplos_3);
+console.log(sum_multiplos_5);
+console.log(sum_multiplos_5+sum_multiplos_3-sum_multiplos_15);
 
